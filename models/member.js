d0703542314b6d4327025804ada313b9ea6b2109
mayuran19 <mayuran19@gmail.com>
2018-09-23 11:11:56 +0800

    user model

---
+++ models/member.js
@@ -0,0 +1,33 @@
+var Sequelize = require('sequelize');
+var bcrypt = require('bcrypt');
+
+var Member = sequelize.define('users', {
+    username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+}, {
+    hooks: {
+      beforeCreate: (user) => {
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(user.password, salt);
+      }
+    },
+    instanceMethods: {
+      validPassword: function(password) {
+        return bcrypt.compareSync(password, this.password);
+      }
+    }    
+});
+
+module.exports = Member;
